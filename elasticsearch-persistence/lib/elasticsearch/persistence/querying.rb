852b71c14adba71e1bab3fc2ed6570dd339616f0
Spencer Markowski <spencer@theablefew.com>
2014-09-11 23:44:16 -0400

    Delegate filter and facet methods

--- elasticsearch-persistence/lib/elasticsearch/persistence/querying.rb
+++ elasticsearch-persistence/lib/elasticsearch/persistence/querying.rb
@@ -2,7 +2,8 @@ module Elasticsearch
   module Persistence
       module Querying
         delegate :first, :first!, :last, :last!, :exists?, :any?, :many?, to: :all
-        delegate :order, :limit, :where, :rewhere, :eager_load, :includes,  :create_with, :none, :unscope, to: :all
+        delegate :order, :limit, :size, :where, :rewhere, :eager_load, :includes,  :create_with, :none, :unscope, to: :all
+        delegate :filter, :facet, :aggregation, to: :all
 
         def fetch(es)
           gateway.search(es.to_elastic, es.search_options)
